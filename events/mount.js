b6c54557ba6925eb87f3215fd8277ef0d2b823c9
forester302 <109694860+forester302@users.noreply.github.com>
2023-03-11 22:51:28 +0000

    Add Player mount support

--- events/mount.js
+++ events/mount.js
@@ -3,17 +3,23 @@ function mount(data) {
 	const message = data.message;
 
 	let Filter = true;
+	let OtherFilter = true;
 
 	if (message.length == 2) {
 		const mobType = message[1].charAt(0).toUpperCase() + message[1].slice(1);
 		Filter = (entity) => entity.mobType === mobType;
+		OtherFilter = (entity) => entity.username == mobType;
 	}
 	else {
 		Filter = (entity) => (entity.type === 'other' && (entity.mobType === 'Boat' || entity.mobType == 'Minecart')) ||
 		entity.type === 'mob' && (entity.mobType == 'Horse' || entity.mobType == 'Donkey' || entity.mobType == 'Mule' ||
 		entity.mobType == 'Llama');
 	}
-	const Entity = bot.nearestEntity(Filter);
+	let Entity = bot.nearestEntity(Filter);
+	if (!Entity && message.length == 2) {
+		Entity = bot.nearestEntity(OtherFilter);
+	}
+	if (!Entity) return;
 	bot.mount(Entity);
 }
 
